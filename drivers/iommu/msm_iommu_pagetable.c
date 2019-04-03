1ffe9fc722b9f7349a4990e96ddc9243b741faac
Nich <nctrenco@gmail.com>
2019-04-03 09:41:11 -0400

    iommu: msm: Replace custom IOMMU flag with upstream version

--- drivers/iommu/msm_iommu_pagetable.c
+++ drivers/iommu/msm_iommu_pagetable.c
@@ -175,7 +175,7 @@ static int __get_pgprot(int prot, int len)
 
 	if (prot & IOMMU_CACHE)
 		tex = (pgprot_val(PAGE_KERNEL) >> 2) & 0x07;
-	else if (prot & IOMMU_DEVICE)
+	else if (prot & IOMMU_MMIO)
 		tex = 0;
 	else
 		tex = msm_iommu_tex_class[MSM_IOMMU_ATTR_NONCACHED];
